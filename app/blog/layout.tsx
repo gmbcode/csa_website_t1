fba5a50db625f63d065458496325f0e36d8778f4
Bhavya Shah <gmbhacademic@gmail.com>
2025-04-07 01:21:46 +0530

    Update layout.tsx

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -29,6 +29,7 @@ export default async function Layout({
   return (
     <html lang="en" className={`${sans.variable} font-sans`}>
       <body className="bg-white dark:bg-zinc-950">
+        <Header name={siteData} />
         {children}
       </body>
     </html>
