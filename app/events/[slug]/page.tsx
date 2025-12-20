f234501a2ef8ffaf43aa3930fe5e597e73fc014b
gmbcode <gmbhacademic@gmail.com>
2025-12-20 13:01:33 +0530

    fix : Fix njs vulnerability - 3

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,20 +1,21 @@
 import { SingleEvent } from "../../../cosmic/blocks/events/SingleEvent";
 
-
 export default async function SingleEventPage({
   params,
   searchParams,
 }: {
   params: Promise<{ slug: string }>;
-  searchParams?: {
-    status: "draft" | "published" | "any";
-  };
+  searchParams?: Promise<{
+    status?: "draft" | "published" | "any";
+  }>;
 }) {
-  const param = await params;
+  const { slug } = await params;
+  const resolvedSearchParams = searchParams ? await searchParams : undefined;
+  
   return (
     <SingleEvent
-      query={{ slug: param.slug, type: "events" }}
-      status={searchParams?.status}
+      query={{ slug: slug, type: "events" }}
+      status={resolvedSearchParams?.status}
     />
   );
 }
