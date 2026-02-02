8f7d9cbe71ad97539907a167a2f3aa5174cd6207
Osh B <oshal.baranwal@gmail.com>
2026-02-03 00:23:19 +0530

    updated /events, formatted ./cosmic/utils.ts

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -3,7 +3,7 @@ export default async function EventListPage() {
   return (
     <div className="bg-white dark:bg-black text-black dark:text-white p-6">
       <h1 className="text-5xl text-center pt-[50px] font-bold mb-6">
-        Upcoming Events
+        All Events
       </h1>
       <EventsList query={{ type: 'events' }} />
     </div>
